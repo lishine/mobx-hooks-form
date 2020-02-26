f14888b31062def9325616fa63c727cd7d9c18a3
Pavel Ravits <vim55k@gmail.com>
2020-02-26 03:33:44 +0200

    test

--- test/test.test.tsx
+++ test/test.test.tsx
@@ -1,11 +1,8 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import { Thing } from '../src';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-// describe('it', () => {
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<Thing />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-// });
+describe('it', () => {
+	it('it is fine', () => {
+		console.log('fine')
+	})
+})
